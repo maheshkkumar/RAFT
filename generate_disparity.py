be910897087657613a39a994c6fddbfea3c26518
maheshkkumar <maheshk2194@gmail.com>
2022-02-25 20:05:06 -0800

    Added script to generate disparity from flow

---
+++ generate_disparity.py
@@ -0,0 +1,47 @@
+import argparse
+import os
+from glob import glob
+
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+
+from core.utils.frame_utils import readFlow
+
+
+def min_max(img):
+    dmin, dmax = img.min(), img.max()
+    img = (img - dmin) / (dmax - dmin)
+    return img
+
+def flow_to_disp(args):
+    
+    # read LR flow files
+    lr_flow_list = sorted(glob(args.lr_flow_dir + '/*left_right.flo'))
+    print(f"Processing {len(lr_flow_list)} LR flow files")
+    
+    for lr_flow_path in tqdm(lr_flow_list, total=len(lr_flow_list)):
+        lr_flow = readFlow(lr_flow_path)
+        
+        disparity = np.clip(1 - (min_max(lr_flow[...,0])), 0.01, 1)
+        
+        # convert disparty to 16-bit int
+        disparity = disparity * (2**16 - 1)
+        
+        # np to PIL 
+        disparity = Image.fromarray(disparity.astype(np.uint16))
+        
+        base_name = os.path.basename(lr_flow_path).split('.')[0].replace('left_right', 'disparity')
+        output_path = os.path.join(args.output_dir, f"{base_name}.png")
+        
+        # save image
+        disparity.save(output_path)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--flow_path', type=str, help='Path to input flow data with L->R flow')
+    parser.add_argument('--output_path', type=str, help='Path to output folder')
+    
+    args = parser.parse_args()
+    flow_to_disp(args)
+    
